ffdafd569286b3f3e15590e59c51907341dea6de
Felix Schaumann <felix.schaumann@annalect.com>
2017-08-21 15:38:54 +0200

    Extract single Todo component and add filters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore } from 'redux';
 
-const Todos = ({items}) => {
+const Todos = ({ items }) => {
   let listItems = items.map((item, index) => {
     if (item.completed) {
       return <li key={index}>#{item.id}: <strike>{item.text}</strike></li>;
@@ -13,54 +13,76 @@ const Todos = ({items}) => {
   return <ul>{listItems}</ul>;
 };
 
-const todos = (state = [], action) => {
+const todo = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: action.completed
-        }
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id === action.id) {
-          return {
-            ...todo,
-            completed: !todo.completed
-          }
-        } else {
-          return todo;
-        }
-      })
+      return {
+        id: action.id,
+        text: action.text,
+        completed: action.completed
+      };
+    case "TOGGLE_TODO":
+      if (state.id === action.id) {
+        return {
+          ...state,
+          completed: !todo.completed
+        };
+      } else {
+        return state;
+      }
     default:
       return state;
   }
 };
 
-let store = createStore(todos);
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case "TOGGLE_TODO":
+      return state.map(t => todo(t, todo));
+    default:
+      return state;
+  }
+};
+
+const filter = (state = 'SHOW_ALL', action) => {
+  switch(action.type) {
+    case 'SET_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    filter: filter(state.filter, action)
+  };
+};
+
+let store = createStore(todoApp);
 
 const render = () => {
-  ReactDOM.render(<Todos items={store.getState()} />,
-                  document.getElementById('app'));
+  ReactDOM.render(
+    <Todos items={store.getState().todos} filter={store.getState().filter} />,
+    document.getElementById("app")
+  );
 };
 
 store.subscribe(render);
 render();
 
-console.log('------- state --------');
-console.log(store.getState());
-console.log('------> dispatch: ADD_TODO');
-store.dispatch({type: 'ADD_TODO', id: 0, text: 'Clean room', completed: true});
-console.log('------- state --------');
-console.log(store.getState());
-console.log('------> dispatch: ADD_TODO');
-store.dispatch({type: 'ADD_TODO', id: 1, text: 'Do dishes', completed: true});
-console.log('------- state --------');
-console.log(store.getState());
-console.log('------> dispatch: TOGGLE_TODO');
-store.dispatch({type: 'TOGGLE_TODO', id: 0});
-console.log('------- state --------');
-console.log(store.getState());
+store.dispatch({
+  type: "ADD_TODO",
+  id: 0,
+  text: "Clean room",
+  completed: true
+});
+store.dispatch({ type: "ADD_TODO", id: 1, text: "Do dishes", completed: true });
+store.dispatch({ type: "TOGGLE_TODO", id: 0 });
+store.dispatch({ type: "SET_FILTER", filter: 'SHOW_COMPLETED'});
